86628d31cfa006b5a117d922f5953b65863d6532
Muhammad Aqib Mehmood <muhammadaqibmehmood73@gmail.com>
2024-08-23 11:30:01 +0500

    First Commit for creating project

---
+++ LeagleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LeagleApp
+//
+//  Created by Mapple.pk on 23/08/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
